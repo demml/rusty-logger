b7bfc6a312b0266331f985b1d7b10082100e94e8
thorrester <steven.forrester@shipt.com>
2023-09-21 21:03:03 +0000

    add more examples

--- examples/json_logger.py
+++ examples/json_logger.py
@@ -1,12 +1,12 @@
-from rusty_logger import Logger, LogLevel, LogConfig, JsonConfig, LogMetadata
+from rusty_logger import Logger, LogLevel, LogConfig, JsonConfig, LogMetadata, LogFileConfig
 
 # This logger will log to stdout at INFO level in json format
 logger = Logger.get_logger(
     name=__file__,
     config=LogConfig(
-        filename="log/test.log",
         level=LogLevel.INFO,
         json_config=JsonConfig(flatten=True),
+        file_config=LogFileConfig(filename="logs/test.log"),
     ),
 )
 
