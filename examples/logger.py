0f488501b27108546c36ee95e084747df02eef30
thorrester <steven.forrester@shipt.com>
2024-01-12 19:38:17 +0000

    update examples

--- examples/logger.py
+++ examples/logger.py
@@ -2,14 +2,20 @@ from rusty_logger import Logger, LogLevel, LogConfig
 
 # Create a logger with default configuration
 # Pushes logs to stdout at INFO level
-logger = Logger.get_logger(name=__file__, config=LogConfig(level=LogLevel.INFO))
+logger = Logger.get_logger(
+    name=__file__,
+    config=LogConfig(
+        level=LogLevel.INFO,
+        thread_id=True,
+    ),
+)
 logger.info("test info")
 
 # Set to ERROR level
 logger.set_level(level=LogLevel.ERROR)
 
 # This should log
-logger.error("test error")
+logger.error("test error", color="red")
 
 # This should not log
 logger.info("test info")
